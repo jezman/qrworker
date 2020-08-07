43bec070e07e34ee7d551ad05065ba5d6c1089cf
Sergey <sgmaslov@yandex.ru>
2020-08-07 12:15:38 +0300

    Description for arg.

--- qrworker.py
+++ qrworker.py
@@ -7,7 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('files', type=argparse.FileType('r'), nargs='+')
 parser.add_argument("-n", "--number", type=int, choices=ALLOWED_NUMBERS,
                     help="number QR codes per page")
-parser.add_argument("-c", "--codes",  action="store_true")
+parser.add_argument("-c", "--codes",  action="store_true",
+                    help="save codes in file")
 
 args = parser.parse_args()
 
