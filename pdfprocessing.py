2597608abab8bff8927b27fdff1a982bea2cb459
Sergey <sgmaslov@yandex.ru>
2020-08-07 11:31:29 +0300

    Splitted 8 QRs per page. Get codes from pdf.

--- pdfprocessing.py
+++ pdfprocessing.py
@@ -1,5 +1,9 @@
 from copy import copy
+from numpy import savetxt
+from pdftotext import PDF as pdf_to_text
 from PyPDF2 import PdfFileWriter, PdfFileReader
+
+IGNORE_LIST = ['МОДЕЛЬ', 'РАЗМЕР', '--', '']
 
 
 def get_coordinations(qr_per_page):
@@ -10,7 +14,8 @@ def get_coordinations(qr_per_page):
         return {'x_left': 21, 'x_right': 190,
                 'y_top': 822, 'y_bottom': 723, 'qr_range': 118}
     elif qr_per_page == 8:
-        pass
+        return {'x_left': 0, 'x_right': 169,
+                'y_top': 844, 'y_bottom': 750, 'qr_range': 102}
 
 
 def split_pdf(filename, qr_per_page):
@@ -39,4 +44,30 @@ def split_pdf(filename, qr_per_page):
 
         with open('Splitted_{}'.format(filename), 'wb') as out_f:
             pdfWriter.write(out_f)
-            print('Complete {}'.format(filename))
+            print('Splitted complete: {}'.format(filename))
+
+
+def parse_pdf(pdf_file):
+    codes = list()
+
+    for page in pdf_file:
+        page = page.split('\n')
+        page = [code.strip() for code in page if code not in IGNORE_LIST]
+
+        # joins codes from two lines
+        two_codes = ([''.join(page[i:i + 2]) for i in range(0, len(page), 2)])
+        [codes.append(code) for code in two_codes]
+
+    return codes
+
+
+def save_codes(pdf_file):
+    with open(pdf_file, "rb") as fl:
+        pdf = pdf_to_text(fl)
+        codes = parse_pdf(pdf)
+        codes = [code.replace('"', '""') for code in codes]
+
+        name_to_save = pdf_file.split('.')[0] + ".txt"
+        savetxt(name_to_save, codes, fmt='"%s"', delimiter=",")
+
+        print('Parse complete, codes saved - {}'.format(name_to_save))
--- qrworker.py
+++ qrworker.py
@@ -1,5 +1,5 @@
 import argparse
-from pdfprocessing import split_pdf
+from pdfprocessing import save_codes, split_pdf
 
 ALLOWED_NUMBERS = [4, 6, 8]
 
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('files', type=argparse.FileType('r'), nargs='+')
 parser.add_argument("-n", "--number", type=int, choices=ALLOWED_NUMBERS,
                     help="number QR codes per page")
+parser.add_argument("-c", "--codes",  action="store_true")
 
 args = parser.parse_args()
 
@@ -14,3 +15,5 @@ args = parser.parse_args()
 if __name__ == "__main__":
     for file in args.files:
         split_pdf(file.name, args.number)
+        if args.codes:
+            save_codes(file.name)
