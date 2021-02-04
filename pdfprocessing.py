5a5802abab3abce5ab811c464ff7e91a5c173347
jezman <j3zm4n@gmail.com>
2021-02-04 08:49:31 +0300

    Remove parse parts.

--- pdfprocessing.py
+++ pdfprocessing.py
@@ -1,9 +1,5 @@
 from copy import copy
-from numpy import savetxt
-from pdftotext import PDF as pdf_to_text
 from PyPDF2 import PdfFileWriter, PdfFileReader
-
-IGNORE_LIST = ['МОДЕЛЬ', 'РАЗМЕР', '--', '']
 
 
 def get_coordinations(qr_per_page):
@@ -45,29 +41,3 @@ def split_pdf(filename, qr_per_page):
         with open('Splitted_{}'.format(filename), 'wb') as out_f:
             pdfWriter.write(out_f)
             print('Splitted complete: {}'.format(filename))
-
-
-def parse_pdf(pdf_file):
-    codes = list()
-
-    for page in pdf_file:
-        page = page.split('\n')
-        page = [code.strip() for code in page if code not in IGNORE_LIST]
-
-        # joins codes from two lines
-        two_codes = ([''.join(page[i:i + 2]) for i in range(0, len(page), 2)])
-        [codes.append(code) for code in two_codes]
-
-    return codes
-
-
-def save_codes(pdf_file):
-    with open(pdf_file, "rb") as fl:
-        pdf = pdf_to_text(fl)
-        codes = parse_pdf(pdf)
-        codes = [code.replace('"', '""') for code in codes]
-
-        name_to_save = pdf_file.split('.')[0] + ".txt"
-        savetxt(name_to_save, codes, fmt='"%s"', delimiter=",")
-
-        print('Parse complete, codes saved - {}'.format(name_to_save))
--- qrworker.py
+++ qrworker.py
@@ -1,5 +1,5 @@
 import argparse
-from pdfprocessing import save_codes, split_pdf
+from pdfprocessing import split_pdf
 
 ALLOWED_NUMBERS = [4, 6, 8]
 
@@ -7,13 +7,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('files', type=argparse.FileType('r'), nargs='+')
 parser.add_argument("-n", "--number", type=int, choices=ALLOWED_NUMBERS,
                     help="number QR codes per page")
-parser.add_argument("-c", "--codes",  action="store_true",
-                    help="save codes in file")
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
     for file in args.files:
         split_pdf(file.name, args.number)
-        if args.codes:
-            save_codes(file.name)
